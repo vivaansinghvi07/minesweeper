f79916367b15e39dd9e2ad968405998a438c4194
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-02-25 16:19:20 -0500

    create Display class to free Main up

---
+++ Display.java
@@ -0,0 +1,12 @@
+public class Display {
+    public static void printIntro() {
+        Text.smoothPrint("Welcome to Minesweeper!\n\n");
+        Text.wait(200);
+        Text.smoothPrint("The objective of this game is to clear the minefield by guessing places until you lose!\n");
+        Text.wait(200);
+        Text.smoothPrint("If you enter the spot on which a mine stands, you lose!\n");
+        Text.wait(200);
+        Text.smoothPrint("To enter a move, simply type in the coordinate of your intended spot, in an <xy> form. An example input would be \"d3\"\n");
+        Text.wait(200);
+    }
+}
