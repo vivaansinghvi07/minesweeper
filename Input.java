486a8044e1ca9c31b20da41d012505179d7a9580
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-02-25 17:27:18 -0500

    added bounded integer input

--- Input.java
+++ Input.java
@@ -15,6 +15,31 @@ public class Input {
         return scan.nextLine();
     }
 
+    // gets the next integer but given that it is within certain bounds
+    public int boundedInt(int lower, int upper) {
+        try {
+
+            // tries to get the next number - if error then its caught
+            int out = scan.nextInt();
+
+            // checks bounds
+            if (out >= lower && out <= upper) {
+                return out;
+            }
+            
+            // prints warning and recursively runs the program
+            Text.smoothPrint("Your input must be within the bounds " + lower + " and " + upper + "!" + "\n");
+            return boundedInt(lower, upper);
+        }
+        catch (Exception e) {
+
+            // prints a prompt and recursively runs the program
+            Text.smoothPrint("Please enter an integer number!\n");
+            return boundedInt(lower, upper);
+
+        }
+    }
+
     // method to close Scanner
     public void close() {
         this.scan.close();
