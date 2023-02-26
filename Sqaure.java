5da96eb064b5f149f6effe6efc35807f713937ca
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-02-26 00:07:12 -0500

    added base template for a square

--- Sqaure.java
+++ Sqaure.java
@@ -1,3 +1,23 @@
 public class Sqaure {
-    
+
+    // variables for the location
+    private int locationX;
+    private int locationY;
+
+    // variables for if its flagged or a mine
+    private boolean flagged;
+    private boolean mine;
+    private boolean hidden;
+
+    // constructor for a square
+    public Sqaure(int x, int y) {
+        // assigns location
+        this.locationX = x;
+        this.locationY = y;
+
+        // assigns starter values
+        this.flagged = false;
+        this.mine = false;
+        this.hidden = true;
+    }
 }
