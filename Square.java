705bcbb88919710daf492fc8512c44abce1bfd1c
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-02-27 13:31:12 -0500

    removed unused variables

--- Square.java
+++ Square.java
@@ -1,9 +1,5 @@
 public class Square {
 
-    // variables for the location
-    private int locationX;
-    private int locationY;
-
     // variables for if its flagged or a mine
     private boolean flagged;
     private boolean mine;
@@ -17,10 +13,7 @@ public class Square {
     private boolean isOpening;
 
     // constructor for a square - y goes first for this project
-    public Square(int y, int x) {
-        // assigns location
-        this.locationX = x;
-        this.locationY = y;
+    public Square() {
 
         // assigns starter values
         this.flagged = false;
