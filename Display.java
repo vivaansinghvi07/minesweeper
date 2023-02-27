0413b6ed0b5621304cff1755d396e40b1756816a
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-02-27 13:09:12 -0500

    changed waits slightly

--- Display.java
+++ Display.java
@@ -12,14 +12,14 @@ public class Display {
         Text.smoothPrint("If you want to flag a location, simply enter the letter \"f\" at the end of your move. If you want to flag the bK square, you would type \"bKf\"\n");
         Text.wait(200);
         Text.smoothPrint("\nThe y-axis is represented by uppercase letters and the x-axis is represented by lowercase letters. Labels are present\n");
-        Text.wait(400);
+        Text.wait(300);
     }
     public static void printFrequency() {
         Text.smoothPrint("How often should mines occur?\n\n");
         Text.wait(200);
         Text.smoothPrint("  1. Rare\n");
         Text.smoothPrint("  2. Often\n");
-        Text.wait(400);
+        Text.wait(300);
         Text.smoothPrint("\nEnter your choice as a number, 1 or 2: ");
     }
     public static void printSize() {
@@ -28,7 +28,7 @@ public class Display {
         Text.smoothPrint("  1. Small\n");
         Text.smoothPrint("  2. Medium\n");
         Text.smoothPrint("  3. Large\n");
-        Text.wait(400);
+        Text.wait(300);
         Text.smoothPrint("\nEnter your choice as an integer, between 1 and 3: ");
     }
 }
